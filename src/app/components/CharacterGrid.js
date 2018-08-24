4058687b9b364c028cc501b961320c344c25da48
Barbara Vasic <barbara.vasic0108@gmail.com>
2018-08-24 12:38:31 +0200

    fix character list and character grid

--- src/app/components/CharacterGrid.js
+++ src/app/components/CharacterGrid.js
@@ -4,26 +4,29 @@ import CharacterCard from './CharacterCard';
 import { getFromStorage } from '../../services/storage-services';
 
 const CharacterGrid = (props) => {
+    const bookmarkedCharacters = getFromStorage("bookmarkedCharacters");
 
-    const renderCharacters = () => {
-        const bookmarkedCharacters = getFromStorage("bookmarkedCharacters");
-        if (bookmarkedCharacters === null && props.searchedCharacters === null 
-            || bookmarkedCharacters && bookmarkedCharacters.length === 0) {
-            return <h4>Type in search</h4>
-        } else {
-            return props.searchedCharacters.map(character => {
-                if (bookmarkedCharacters) {
-                    const bookmarked = bookmarkedCharacters.find(bookmarkedCharacter => bookmarkedCharacter.id === character.id)
-                    return (bookmarked === undefined) ? <CharacterCard character={character} key={character.id} />
-                        : <CharacterCard character={bookmarked} key={bookmarked.id} />
-                } else {
-                    return <CharacterCard character={character} key={character.id} />
-                }
-            })
-        }
+    const renderCharacterCards = () => {
+        return props.searchedCharacters.map(character => {
+            if (bookmarkedCharacters) {
+                const bookmarked = bookmarkedCharacters.find(bookmarkedCharacter => bookmarkedCharacter.id === character.id)
+                return (bookmarked === undefined) ? <CharacterCard character={character} key={character.id} />
+                    : <CharacterCard character={bookmarked} key={bookmarked.id} />
+            } else {
+                return <CharacterCard character={character} key={character.id} />
+            }
+
+        })
     }
-
-
+    const renderCharacters = () => {
+           if (props.searchedCharacters) {
+               return renderCharacterCards() 
+            } else if (bookmarkedCharacters && !props.searchedCharacters && bookmarkedCharacters.length) {
+                return <h4>Type in search</h4>
+            } else {
+                return <h4>Type in search</h4>
+            }
+    }
 
     return (
         <main className="container-grid">
--- src/app/components/CharacterList.js
+++ src/app/components/CharacterList.js
@@ -6,22 +6,28 @@ import { getFromStorage } from '../../services/storage-services';
 
 const CharacterList = (props) => {
 
+    const bookmarkedCharacters = getFromStorage("bookmarkedCharacters");
+
+    const renderCharacterList = () => {
+        return props.searchedCharacters.map(character => {
+            if (bookmarkedCharacters) {
+                const bookmarked = bookmarkedCharacters.find(bookmarkedCharacter => bookmarkedCharacter.id === character.id)
+                return (bookmarked === undefined) ? <CharacterItem character={character} key={character.id} />
+                    : <CharacterItem character={bookmarked} key={bookmarked.id} />
+            } else {
+                return <CharacterItem character={character} key={character.id} />
+            }
+        })
+    }
+    
     const renderCharacters = () => {
-        const bookmarkedCharacters = getFromStorage("bookmarkedCharacters"); 
-        if (bookmarkedCharacters === null && props.searchedCharacters === null
-            || bookmarkedCharacters && bookmarkedCharacters.length === 0) {
-            return <h4>Type in search</h4>
-        } else {
-            return props.searchedCharacters.map(character => {
-                if (bookmarkedCharacters) {
-                    const bookmarked = bookmarkedCharacters.find(bookmarkedCharacter => bookmarkedCharacter.id === character.id)
-                    return (bookmarked === undefined) ? <CharacterItem character={character} key={character.id} />
-                        : <CharacterItem character={bookmarked} key={bookmarked.id} />
-                } else {
-                    return <CharacterItem character={character} key={character.id} />
-                }
-            })
-        }
+           if (props.searchedCharacters) {
+               return renderCharacterList() 
+            } else if (bookmarkedCharacters && !props.searchedCharacters && bookmarkedCharacters.length) {
+                return <h4>Type in search</h4>
+            } else {
+                return <h4>Type in search</h4>
+            }
     }
     return (
         <div className="container-list">
